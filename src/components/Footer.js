4e270aebe8af6b5ef29762f7e690ceff95853449
introbond <introbond.dev@gmail.com>
2022-10-19 17:42:33 +0700

    basic structure

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import IconGithub from "@material-ui/icons/GitHub";
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <div>
+                <IconGithub />
+            </div>
+            <p> &copy; 2022 introbond.com </p>
+        </div>
+    )
+}
+
+export default Footer;
