5e7073e1fe8b4c80455ad215c1b7151c0497e287
introbond <introbond.dev@gmail.com>
2022-10-23 13:42:35 +0700

    add cirtifications

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import ReorderIcon from "@material-ui/icons/Reorder"
-
 import "../styles/Navbar.css";
 
 const Navbar = () => {
     const [expandNavbar, setExpandNavbar] = useState(false);
     const location = useLocation();
+    const openInNewTab = (e) => {
+        window.open(e, '_blank', 'noopener,noreferrer')
+    }
 
     useEffect(() => {
         setExpandNavbar(false)
@@ -24,6 +26,7 @@ const Navbar = () => {
                 <Link to="/skills"> Skills </Link>
                 <Link to="/projects"> Projects </Link>
                 <Link to="/experience"> Experience </Link>
+                <Link to="#" onClick={() => openInNewTab('https://introbond.github.io/micro-app-cir-slide/')} > Cirtifications </Link>
             </div>
         </div>
     )
